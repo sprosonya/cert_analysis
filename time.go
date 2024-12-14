30c71a9709577663da36017ce764295bdb575e1d
Софья <[sonya_pr@bk.ru]>
2024-12-14 20:07:32 +0300

    new version

--- time.go
+++ time.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/base64"
 	"encoding/csv"
 	"encoding/json"
 	"fmt"
@@ -12,6 +13,7 @@ import (
 	ctX509 "github.com/google/certificate-transparency-go/x509"
 	"io"
 	logger "log"
+	"math"
 	"net/http"
 	"os"
 	"sort"
@@ -29,6 +31,7 @@ type LogTime struct {
 	log      Log
 	certType string
 	time     time.Time
+	index    int64
 }
 
 type InfoAboutTime struct {
@@ -37,9 +40,11 @@ type InfoAboutTime struct {
 }
 
 type Log struct {
-	Name string
-	Url  string
-	Year int
+	Name  string
+	Url   string
+	Year  int
+	start time.Time
+	end   time.Time
 }
 
 type LogData struct {
@@ -50,6 +55,7 @@ type StrangeCase1Log struct {
 	info     InfoAboutTime
 	log      Log
 	timeLog  time.Time
+	index    int64
 	dif      time.Duration
 	reason   string
 	typeCert string
@@ -67,19 +73,21 @@ type SmallDifLogs struct {
 	typeCert2 string
 	timeLog1  time.Time
 	timeLog2  time.Time
+	index1    int64
+	index2    int64
 	dif       time.Duration
 }
 
 var logs = [9]Log{
-	{"Yandex", "https://ct-agate.yandex.net/2025/", 2025},
-	{"VK", "https://ctlog2025.mail.ru/nca2025/", 2025},
-	{"MDC", "https://25.ctlog.digital.gov.ru/2025/", 2025},
-	{"MDC", "https://24.ctlog.digital.gov.ru/2024/", 2024},
-	{"VK", "https://ctlog2024.mail.ru/nca2024/", 2024},
-	{"Yandex", "https://ct-agate.yandex.net/2024/", 2024},
-	{"VK", "https://ctlog2023.mail.ru/nca2023/", 2023},
-	{"Yandex", "https://ct-agate.yandex.net/2023/", 2023},
-	{"Yandex", "https://ct-agate.yandex.net/2022/", 2022},
+	{"Yandex", "https://ct-agate.yandex.net/2025/", 2025, time.Date(2025, time.January, 1, 0, 0, 0, 0, time.UTC), time.Date(2025, time.December, 31, 23, 59, 59, 0, time.UTC)},
+	{"VK", "https://ctlog2025.mail.ru/nca2025/", 2025, time.Date(2025, time.January, 1, 0, 0, 0, 0, time.UTC), time.Date(2025, time.December, 31, 23, 59, 59, 0, time.UTC)},
+	{"MDC", "https://25.ctlog.digital.gov.ru/2025/", 2025, time.Date(2025, time.January, 1, 0, 0, 0, 0, time.UTC), time.Date(2025, time.December, 31, 23, 59, 59, 0, time.UTC)},
+	{"MDC", "https://24.ctlog.digital.gov.ru/2024/", 2024, time.Date(2024, time.January, 1, 0, 0, 0, 0, time.UTC), time.Date(2024, time.December, 31, 23, 59, 59, 0, time.UTC)},
+	{"VK", "https://ctlog2024.mail.ru/nca2024/", 2024, time.Date(2024, time.January, 1, 0, 0, 0, 0, time.UTC), time.Date(2024, time.December, 31, 23, 59, 59, 0, time.UTC)},
+	{"Yandex", "https://ct-agate.yandex.net/2024/", 2024, time.Date(2024, time.January, 1, 0, 0, 0, 0, time.UTC), time.Date(2024, time.December, 31, 23, 59, 59, 0, time.UTC)},
+	{"VK", "https://ctlog2023.mail.ru/nca2023/", 2023, time.Date(2023, time.January, 1, 0, 0, 0, 0, time.UTC), time.Date(2023, time.December, 31, 23, 59, 59, 0, time.UTC)},
+	{"Yandex", "https://ct-agate.yandex.net/2023/", 2023, time.Date(2023, time.January, 1, 0, 0, 0, 0, time.UTC), time.Date(2024, time.February, 1, 0, 0, 0, 0, time.UTC)},
+	{"Yandex", "https://ct-agate.yandex.net/2022/", 2022, time.Date(2022, time.January, 1, 0, 0, 0, 0, time.UTC), time.Date(2023, time.February, 1, 0, 0, 0, 0, time.UTC)},
 }
 
 func getLenOfLog(url string) int64 {
@@ -119,14 +127,14 @@ func getAndParseCerts(startIndex int64, endIndex int64, masInfo map[string]*Info
 	var i int64
 	for i = startIndex; i < endIndex; i += 50 {
 		certs := getCerts(i, i+49, log.Url)
-		for _, certificate := range certs.Entries {
-			parsedEntry, err := ct.LogEntryFromLeaf(1, &certificate)
+		for j, leafEntry := range certs.Entries {
+			parsedEntry, err := ct.LogEntryFromLeaf(i+int64(j), &leafEntry)
 			if err != nil {
 				fmt.Printf(err.Error())
 			}
 			var cert *ctX509.Certificate
 			if parsedEntry.X509Cert != nil {
-				logTime := LogTime{log, "cert", time.UnixMilli(int64(parsedEntry.Leaf.TimestampedEntry.Timestamp))}
+				logTime := LogTime{log, "cert", time.UnixMilli(int64(parsedEntry.Leaf.TimestampedEntry.Timestamp)), parsedEntry.Index}
 				cert = parsedEntry.X509Cert
 				info, ok := masInfo[cert.SerialNumber.String()]
 				if ok {
@@ -138,7 +146,7 @@ func getAndParseCerts(startIndex int64, endIndex int64, masInfo map[string]*Info
 					masInfo[cert.SerialNumber.String()] = info
 				}
 			} else if parsedEntry.Precert != nil {
-				logTime := LogTime{log, "precert", time.UnixMilli(int64(parsedEntry.Leaf.TimestampedEntry.Timestamp))}
+				logTime := LogTime{log, "precert", time.UnixMilli(int64(parsedEntry.Leaf.TimestampedEntry.Timestamp)), parsedEntry.Index}
 				cert, err = ctX509.ParseCertificate(parsedEntry.Precert.Submitted.Data)
 				if err != nil {
 					logger.Println("Error parsing precert:", err)
@@ -178,7 +186,7 @@ func analysisDifBetweenLogs(mas map[string]*InfoAboutTime) []StrangeCaseLogs {
 			for j := i + 1; j < len(info.mas); j++ {
 				dif := info.mas[i].time.UTC().Sub(info.mas[j].time.UTC())
 				if dif.Milliseconds() == 0 {
-					smallDif := SmallDifLogs{log1: info.mas[i].log, log2: info.mas[j].log, timeLog1: info.mas[i].time.UTC(), timeLog2: info.mas[j].time.UTC(), dif: dif, typeCert1: info.mas[i].certType, typeCert2: info.mas[j].certType}
+					smallDif := SmallDifLogs{log1: info.mas[i].log, log2: info.mas[j].log, timeLog1: info.mas[i].time.UTC(), timeLog2: info.mas[j].time.UTC(), dif: dif, typeCert1: info.mas[i].certType, typeCert2: info.mas[j].certType, index1: info.mas[i].index, index2: info.mas[j].index}
 					strangeCase.difs = append(strangeCase.difs, smallDif)
 					caseManyLogs = append(caseManyLogs, strangeCase)
 				}
@@ -192,33 +200,117 @@ func analysisDifBetweenLogAndCert(mas map[string]*InfoAboutTime) []StrangeCase1L
 	cases := make([]StrangeCase1Log, 0)
 	for _, info := range mas {
 		for _, logInfo := range info.mas {
-			startOfYear := time.Date(logInfo.log.Year, time.January, 1, 0, 0, 0, 0, time.UTC)
-			endOfYear := time.Date(logInfo.log.Year, time.December, 31, 23, 59, 59, 0, time.UTC)
-			if info.cert.NotAfter.UTC().Before(startOfYear) || info.cert.NotAfter.UTC().After(endOfYear) {
+			if info.cert.NotAfter.UTC().Before(logInfo.log.start) || info.cert.NotAfter.UTC().After(logInfo.log.end) {
 				var dif time.Duration
-				if info.cert.NotAfter.UTC().Before(startOfYear) {
-					dif = startOfYear.Sub(info.cert.NotAfter.UTC())
+				if info.cert.NotAfter.UTC().Before(logInfo.log.start) {
+					dif = logInfo.log.start.Sub(info.cert.NotAfter.UTC())
 				} else {
-					dif = info.cert.NotAfter.UTC().Sub(endOfYear)
+					dif = info.cert.NotAfter.UTC().Sub(logInfo.log.end)
 				}
-				strangeCase := StrangeCase1Log{info: *info, log: logInfo.log, timeLog: logInfo.time.UTC(), reason: "wrongLog", typeCert: logInfo.certType, dif: dif}
+				strangeCase := StrangeCase1Log{info: *info, log: logInfo.log, timeLog: logInfo.time.UTC(), reason: "wrongLog", typeCert: logInfo.certType, dif: dif, index: logInfo.index}
 				cases = append(cases, strangeCase)
 			}
 
 			if info.cert.NotBefore.UTC().After(logInfo.time.UTC()) {
-				strangeCase := StrangeCase1Log{info: *info, log: logInfo.log, timeLog: logInfo.time.UTC(), reason: "timeInLogEarlierThanReleaseTime", typeCert: logInfo.certType, dif: info.cert.NotBefore.UTC().Sub(logInfo.time.UTC())}
+				strangeCase := StrangeCase1Log{info: *info, log: logInfo.log, timeLog: logInfo.time.UTC(), reason: "timeInLogEarlierThanReleaseTime", typeCert: logInfo.certType, dif: info.cert.NotBefore.UTC().Sub(logInfo.time.UTC()), index: logInfo.index}
 				cases = append(cases, strangeCase)
 				continue
 			}
 
 			dif := logInfo.time.UTC().Sub(info.cert.NotBefore.UTC())
 			if dif.Milliseconds() <= 50 {
-				strangeCase := StrangeCase1Log{info: *info, log: logInfo.log, timeLog: logInfo.time.UTC(), dif: dif, reason: "tooSmallDifBetweenReleaseAndLogTime", typeCert: logInfo.certType}
+				strangeCase := StrangeCase1Log{info: *info, log: logInfo.log, timeLog: logInfo.time.UTC(), dif: dif, reason: "tooSmallDifBetweenReleaseAndLogTime", typeCert: logInfo.certType, index: logInfo.index}
 				cases = append(cases, strangeCase)
 			}
 		}
 	}
 	return cases
+}
+func buildingHistogramBetweenLogs(mas map[string]*InfoAboutTime) *charts.Bar {
+	difs := make(map[int]int)
+	maxValue := 0
+	for _, info := range mas {
+		var maxDif time.Duration
+		for i := 0; i < len(info.mas); i++ {
+			for j := i + 1; j < len(info.mas); j++ {
+				dif1 := info.mas[i].time.UTC().Sub(info.mas[j].time.UTC())
+				dif2 := info.mas[j].time.UTC().Sub(info.mas[i].time.UTC())
+				maxDifCur := max(dif1, dif2)
+				if maxDifCur > maxDif {
+					maxDif = maxDifCur
+				}
+				if int(math.Floor(maxDifCur.Seconds())) > maxValue {
+					maxValue = int(math.Ceil(maxDifCur.Seconds()))
+				}
+			}
+		}
+		difs[int(math.Floor(maxDif.Seconds()))] += 1
+	}
+	barNames := make([]string, 0)
+	items := make([]opts.BarData, 0)
+	for i := 0; i <= maxValue; i++ {
+		n, ok := difs[i]
+		if ok {
+			barNames = append(barNames, strconv.Itoa(i)+"≤n<"+strconv.Itoa(i+1))
+			items = append(items, opts.BarData{Value: n})
+		}
+	}
+	bar := newBar("Varying of LogTime difference")
+	bar.SetXAxis(barNames).AddSeries("number of certificates in logs", items)
+	bar.SetGlobalOptions(
+		charts.WithYAxisOpts(opts.YAxis{Type: "value"}),
+		charts.WithDataZoomOpts(opts.DataZoom{
+			Type:  "slider",
+			Start: 0,
+			End:   100,
+		}),
+		charts.WithXAxisOpts(opts.XAxis{
+			Name: "interval, sec",
+		}))
+	return bar
+}
+func buildingHistogramBetweenLogAndCert(mas map[string]*InfoAboutTime) *charts.Bar {
+	difs := make(map[int]int)
+	maxValue := -10000000000
+	minValue := 1000000000
+	for _, info := range mas {
+		minDif := 100000000
+		for _, logInfo := range info.mas {
+			dif := logInfo.time.UTC().Sub(info.cert.NotBefore.UTC())
+			if int(math.Floor(dif.Seconds())) < minDif {
+				minDif = int(math.Floor(dif.Seconds()))
+			}
+			if int(math.Floor(dif.Seconds())) > maxValue {
+				maxValue = int(math.Floor(dif.Seconds()))
+			}
+			if int(math.Floor(dif.Seconds())) < minValue {
+				minValue = int(math.Floor(dif.Seconds()))
+			}
+		}
+		difs[minDif] += 1
+	}
+	barNames := make([]string, 0)
+	items := make([]opts.BarData, 0)
+	for i := minValue; i <= maxValue; i++ {
+		n, ok := difs[i]
+		if ok {
+			barNames = append(barNames, strconv.Itoa(i)+"≤n<"+strconv.Itoa(i+1))
+			items = append(items, opts.BarData{Value: n})
+		}
+	}
+	bar := newBar("Varying of LogTime and ReleaseTime difference")
+	bar.SetXAxis(barNames).AddSeries("number of certificates", items)
+	bar.SetGlobalOptions(
+		charts.WithYAxisOpts(opts.YAxis{Type: "value"}),
+		charts.WithDataZoomOpts(opts.DataZoom{
+			Type:  "slider",
+			Start: 0,
+			End:   100,
+		}),
+		charts.WithXAxisOpts(opts.XAxis{
+			Name: "interval, sec",
+		}))
+	return bar
 }
 
 func analysisTimeInLog(mas map[string]*InfoAboutTime) *charts.Bar {
@@ -234,9 +326,13 @@ func analysisTimeInLog(mas map[string]*InfoAboutTime) *charts.Bar {
 		barNames = append(barNames, strconv.Itoa(i))
 		items = append(items, opts.BarData{Value: len(hours[i])})
 	}
-	bar := newBar("Log Time")
+	bar := newBar("Log Time (UTC)")
 	bar.SetXAxis(barNames).
-		AddSeries("", items)
+		AddSeries("number of certificates in logs", items)
+	bar.SetGlobalOptions(charts.WithXAxisOpts(opts.XAxis{
+		Name: "hours"}),
+		charts.WithYAxisOpts(opts.YAxis{Type: "value"}),
+	)
 	return bar
 }
 
@@ -251,9 +347,12 @@ func analysisTimeRelease(mas map[string]*InfoAboutTime) *charts.Bar {
 		barNames = append(barNames, strconv.Itoa(i))
 		items = append(items, opts.BarData{Value: len(hours[i])})
 	}
-	bar := newBar("Release Time")
+	bar := newBar("Release Time (UTC)")
 	bar.SetXAxis(barNames).
-		AddSeries("", items)
+		AddSeries("number of certificates", items)
+	bar.SetGlobalOptions(charts.WithXAxisOpts(opts.XAxis{
+		Name: "hours"}),
+		charts.WithYAxisOpts(opts.YAxis{Type: "value"}))
 	return bar
 }
 
@@ -263,6 +362,8 @@ func plot(mas map[string]*InfoAboutTime, html string) {
 	page.AddCharts(
 		analysisTimeInLog(mas),
 		analysisTimeRelease(mas),
+		buildingHistogramBetweenLogs(mas),
+		buildingHistogramBetweenLogAndCert(mas),
 	)
 	f, err := os.Create("plots.html")
 	if err != nil {
@@ -304,11 +405,22 @@ func createCSVsData(mas1Log []StrangeCase1Log, masManyLogs []StrangeCaseLogs) ([
 		return mas1Log[i].dif > mas1Log[j].dif
 	})
 	dataWrongLog := [][]string{
-		{"CommonName", "Type", "CA", "SerialNumber", "LogName", "LogYear", "ExpireTime", "LogTime", "Difference"},
+		{"CommonName", "Type", "CA", "SerialNumber", "LogIndex", "LogName", "LogYear", "ExpireTime", "Difference", "RawCertificate"},
 	}
 	for _, info := range mas1Log {
 		if info.reason == "wrongLog" {
-			str := []string{info.info.cert.Subject.CommonName, info.typeCert, info.info.cert.Issuer.CommonName, fmt.Sprintf("%X", info.info.cert.SerialNumber), info.log.Name, strconv.Itoa(info.log.Year), info.info.cert.NotAfter.UTC().Format("2006-01-02 15:04:05.000"), info.timeLog.UTC().Format("2006-01-02 15:04:05.000"), info.dif.String()}
+			str := []string{
+				info.info.cert.Subject.CommonName,
+				info.typeCert,
+				info.info.cert.Issuer.CommonName,
+				fmt.Sprintf("%X", info.info.cert.SerialNumber),
+				fmt.Sprint(info.index),
+				info.log.Name,
+				strconv.Itoa(info.log.Year),
+				info.info.cert.NotAfter.UTC().Format("2006-01-02 15:04:05.000"),
+				info.dif.String(),
+				base64.StdEncoding.EncodeToString(info.info.cert.Raw),
+			}
 			dataWrongLog = append(dataWrongLog, str)
 		}
 	}
@@ -318,11 +430,23 @@ func createCSVsData(mas1Log []StrangeCase1Log, masManyLogs []StrangeCaseLogs) ([
 		return mas1Log[i].dif > mas1Log[j].dif
 	})
 	dataLogTimeEarlierThanRelease := [][]string{
-		{"CommonName", "Type", "CA", "SerialNumber", "LogName", "LogYear", "LogTime", "ReleaseTime", "Difference"},
+		{"CommonName", "Type", "CA", "SerialNumber", "LogIndex", "LogName", "LogYear", "LogTime", "ReleaseTime", "Difference", "RawCertificate"},
 	}
 	for _, info := range mas1Log {
 		if info.reason == "timeInLogEarlierThanReleaseTime" {
-			str := []string{info.info.cert.Subject.CommonName, info.typeCert, info.info.cert.Issuer.CommonName, fmt.Sprintf("%X", info.info.cert.SerialNumber), info.log.Name, strconv.Itoa(info.log.Year), info.timeLog.UTC().Format("2006-01-02 15:04:05.000"), info.info.cert.NotBefore.UTC().Format("2006-01-02 15:04:05.000"), info.dif.String()}
+			str := []string{
+				info.info.cert.Subject.CommonName,
+				info.typeCert,
+				info.info.cert.Issuer.CommonName,
+				fmt.Sprintf("%X", info.info.cert.SerialNumber),
+				fmt.Sprint(info.index),
+				info.log.Name,
+				strconv.Itoa(info.log.Year),
+				info.timeLog.UTC().Format("2006-01-02 15:04:05.000"),
+				info.info.cert.NotBefore.UTC().Format("2006-01-02 15:04:05.000"),
+				info.dif.String(),
+				base64.StdEncoding.EncodeToString(info.info.cert.Raw),
+			}
 			dataLogTimeEarlierThanRelease = append(dataLogTimeEarlierThanRelease, str)
 		}
 	}
@@ -332,22 +456,48 @@ func createCSVsData(mas1Log []StrangeCase1Log, masManyLogs []StrangeCaseLogs) ([
 		return mas1Log[i].dif < mas1Log[j].dif
 	})
 	dataTooSmallDif := [][]string{
-		{"CommonName", "Type", "CA", "SerialNumber", "LogName", "LogYear", "LogTime", "ReleaseTime", "Difference"},
+		{"CommonName", "Type", "CA", "SerialNumber", "LogIndex", "LogName", "LogYear", "LogTime", "ReleaseTime", "Difference", "RawCertificate"},
 	}
 	for _, info := range mas1Log {
 		if info.reason == "tooSmallDifBetweenReleaseAndLogTime" {
-			str := []string{info.info.cert.Subject.CommonName, info.typeCert, info.info.cert.Issuer.CommonName, fmt.Sprintf("%X", info.info.cert.SerialNumber), info.log.Name, strconv.Itoa(info.log.Year), info.timeLog.UTC().Format("2006-01-02 15:04:05.000"), info.info.cert.NotBefore.UTC().Format("2006-01-02 15:04:05.000"), info.dif.String()}
+			str := []string{
+				info.info.cert.Subject.CommonName,
+				info.typeCert,
+				info.info.cert.Issuer.CommonName,
+				fmt.Sprintf("%X", info.info.cert.SerialNumber),
+				fmt.Sprint(info.index),
+				info.log.Name,
+				strconv.Itoa(info.log.Year),
+				info.timeLog.UTC().Format("2006-01-02 15:04:05.000"),
+				info.info.cert.NotBefore.UTC().Format("2006-01-02 15:04:05.000"),
+				info.dif.String(),
+				base64.StdEncoding.EncodeToString(info.info.cert.Raw)}
 			dataTooSmallDif = append(dataTooSmallDif, str)
 		}
 	}
 
 	//equalTimeInLogs
 	dataEqualTimeLogs := [][]string{
-		{"CommonName", "CA", "SerialNumber", "LogName1", "LogYear1", "Type1", "LogTime1", "LogName2", "LogYear2", "Type2", "LogTime2"},
+		{"CommonName", "CA", "SerialNumber", "LogIndex1", "LogName1", "LogYear1", "Type1", "LogTime1", "LogIndex2", "LogName2", "LogYear2", "Type2", "LogTime2", "RawCertificate"},
 	}
 	for _, info := range masManyLogs {
 		for _, dif := range info.difs {
-			str := []string{info.info.cert.Subject.CommonName, info.info.cert.Issuer.CommonName, fmt.Sprintf("%X", info.info.cert.SerialNumber), dif.log1.Name, strconv.Itoa(dif.log1.Year), dif.typeCert1, dif.timeLog1.UTC().Format("2006-01-02 15:04:05.000"), dif.log2.Name, strconv.Itoa(dif.log2.Year), dif.typeCert2, dif.timeLog2.UTC().Format("2006-01-02 15:04:05.000")}
+			str := []string{
+				info.info.cert.Subject.CommonName,
+				info.info.cert.Issuer.CommonName,
+				fmt.Sprintf("%X", info.info.cert.SerialNumber),
+				fmt.Sprint(dif.index1),
+				dif.log1.Name,
+				strconv.Itoa(dif.log1.Year),
+				dif.typeCert1,
+				dif.timeLog1.UTC().Format("2006-01-02 15:04:05.000"),
+				fmt.Sprint(dif.index2),
+				dif.log2.Name,
+				strconv.Itoa(dif.log2.Year),
+				dif.typeCert2,
+				dif.timeLog2.UTC().Format("2006-01-02 15:04:05.000"),
+				base64.StdEncoding.EncodeToString(info.info.cert.Raw),
+			}
 			dataEqualTimeLogs = append(dataEqualTimeLogs, str)
 		}
 	}
